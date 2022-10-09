4f21d6d90f95ca3e88590dd3f1752144a55009de
Starofall <starofall@gmail.com>
2022-10-09 09:20:48 +0200

    fix playSanity

--- src/LovecraftLetter/ValidPlayableMoves.hs
+++ src/LovecraftLetter/ValidPlayableMoves.hs
@@ -7,7 +7,7 @@ import Data.List (nub)
 
 data PlayableMove = PlayableMove
   { card :: Card,
-    playCrazy :: Bool,
+    playSanity :: Sanity,
     target :: Maybe PlayerId,
     guess :: Maybe Int
     -- crazy 6 is complex
@@ -35,24 +35,24 @@ getCurrentPlayerState rs@(RoundState _ currentPlayerId _) =
 listCardPlayableMoves :: RoundState -> Card -> [PlayableMove]
 listCardPlayableMoves rs c =
   case (cardValue c, cardSanity c) of
-    (1, Sane) -> [PlayableMove c False (Just $ fromInteger $ toInteger playerId) (Just guess)
+    (1, Sane) -> [PlayableMove c Sane (Just $ fromInteger $ toInteger playerId) (Just guess)
       | guess <- [2..8], playerId <- otherPlayerIds] -- for each player that is not me, 2-8
-    (2, Sane) -> [PlayableMove c False (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
-    (3, Sane) -> [PlayableMove c False (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
-    (4, Sane) -> [PlayableMove c False Nothing Nothing]
-    (5, Sane) -> [PlayableMove c False (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
-    (6, Sane) -> [PlayableMove c False (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
-    (7, Sane) -> [PlayableMove c False Nothing Nothing]
-    (8, Sane) -> [PlayableMove c False Nothing Nothing]
-    (0, Insane) -> [(PlayableMove c True Nothing Nothing), (PlayableMove c False Nothing Nothing)]
-    (1, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) (Just guess) | guess <- [2..8], playerId <- otherPlayerIds, sane <- [True,False]]
-    (2, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds, sane <- [True,False]]
-    (3, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds, sane <- [True,False]]
-    (4, Insane) -> [(PlayableMove c True Nothing Nothing), (PlayableMove c False Nothing Nothing)]
-    (5, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds, sane <- [True,False]]
-    (6, Insane) -> [PlayableMove c True Nothing Nothing] ++ [PlayableMove c False (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
-    (7, Insane) -> [(PlayableMove c False Nothing Nothing),(PlayableMove c False Nothing Nothing)]
-    (8, Insane) -> [(PlayableMove c False Nothing Nothing),(PlayableMove c False Nothing Nothing)]
+    (2, Sane) -> [PlayableMove c Sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
+    (3, Sane) -> [PlayableMove c Sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
+    (4, Sane) -> [PlayableMove c Sane Nothing Nothing]
+    (5, Sane) -> [PlayableMove c Sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
+    (6, Sane) -> [PlayableMove c Sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
+    (7, Sane) -> [PlayableMove c Sane Nothing Nothing]
+    (8, Sane) -> [PlayableMove c Sane Nothing Nothing]
+    (0, Insane) -> [(PlayableMove c Insane Nothing Nothing), (PlayableMove c Sane Nothing Nothing)]
+    (1, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) (Just guess) | guess <- [2..8], playerId <- otherPlayerIds, sane <- [Sane,Insane]]
+    (2, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds, sane <- [Sane,Insane]]
+    (3, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds, sane <- [Sane,Insane]]
+    (4, Insane) -> [(PlayableMove c Insane Nothing Nothing), (PlayableMove c Sane Nothing Nothing)]
+    (5, Insane) -> [PlayableMove c sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds, sane <- [Sane,Insane]]
+    (6, Insane) -> [PlayableMove c Insane Nothing Nothing] ++ [PlayableMove c Sane (Just $ fromInteger $ toInteger playerId) Nothing | playerId <- otherPlayerIds]
+    (7, Insane) -> [(PlayableMove c Sane Nothing Nothing),(PlayableMove c Insane Nothing Nothing)]
+    (8, Insane) -> [(PlayableMove c Sane Nothing Nothing),(PlayableMove c Insane Nothing Nothing)]
     _ -> error "Invalid"
   where
     playerStates = roundPlayerStates rs
